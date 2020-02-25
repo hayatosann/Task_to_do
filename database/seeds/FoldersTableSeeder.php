4bba4f24a8c7400c475621ddcec997163c3d4b37
SasanoHayato <sasano.hayato@gmail.com>
2020-02-25 14:19:26 +0800

    Set sample data in folder table

--- database/seeds/FoldersTableSeeder.php
+++ database/seeds/FoldersTableSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
 
 class FoldersTableSeeder extends Seeder
 {
@@ -14,11 +17,11 @@ class FoldersTableSeeder extends Seeder
         //
         $titles = ['プライベート', '仕事', '旅行'];
 
-        foreach ($titles as $title){
+        foreach ($titles as $title) {
             DB::table('folders')->insert([
-                'title'-> $title,
-                'created_at'-> Carbon::now(),
-                'updated_at'-> Carbon::now(),
+                'title' => $title,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
             ]);
         }
     }
