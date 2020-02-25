41f654992cdfa2432c159159e4543bd0eb974259
SasanoHayato <sasano.hayato@gmail.com>
2020-02-25 21:26:03 +0800

    write down description

--- app/Http/Requests/CreateFolder.php
+++ app/Http/Requests/CreateFolder.php
@@ -13,7 +13,7 @@ class CreateFolder extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -25,6 +25,7 @@ class CreateFolder extends FormRequest
     {
         return [
             //
+            'title' => 'required',
         ];
     }
 }
