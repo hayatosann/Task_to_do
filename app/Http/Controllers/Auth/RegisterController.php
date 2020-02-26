f345e771c1d65155b03436fc58650df9b083e27c
SasanoHayato <sasano.hayato@gmail.com>
2020-02-27 01:34:22 +0800

    Set redirect

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,8 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -53,6 +54,10 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ],[],[
+            'name'=>'ユーザー名',
+            'email'=>'メールアドレス',
+            'password'=>'パスワード',
         ]);
     }
 
