64b1a11387fd6bbcb0bb1127ea2117a889b5ebc6
SasanoHayato <sasano.hayato@gmail.com>
2020-02-27 01:57:42 +0800

    set route

--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,8 @@ Route::post('/folders/{id}/tasks/create', 'TaskController@create');
 
 Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
 Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+
+Route::get('/', 'HomeController@index')->name('home');
+
+Auth::routes();
 ?>
