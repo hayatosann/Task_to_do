26e9aca7a6c6fc04e87b7a4254701aeb65637d15
SasanoHayato <sasano.hayato@gmail.com>
2020-02-26 09:25:16 +0800

    Create edit file

---
+++ resources/views/tasks/edit.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('styles')
+  @include('share.flatpickr.styles')
+@endsection
+
+@section('content')
+  <!-- 変更なし -->
+@endsection
+
+@section('scripts')
+  @include('share.flatpickr.scripts')
+@endsection
